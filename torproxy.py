3520bd05d703295066b93dce6f2fd76e829eb52b
Arnaud Durand <arnaud.durand@live.com>
2015-11-24 04:55:52 +0100

    Spinning up the hamster...

--- torproxy.py
+++ torproxy.py
@@ -18,10 +18,58 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import os
 import re
+import socket
+import time
 
+import socks
+import stem.connection
+import stem.process
+import stem.socket
 from scrapy import log
+from scrapy.core.downloader.handlers.http11 import (HTTP11DownloadHandler,
+                                                    ScrapyAgent)
+from scrapy.core.downloader.webclient import _parse
 from scrapy.utils.request import request_fingerprint
+from scrapy.xlib.tx import TCP4ClientEndpoint
+from stem import Signal
+from stem.control import Controller
+from stem.util import term
+from twisted.internet import reactor
+from txsocksx.http import SOCKS5Agent
+
+
+class Socks5DownloadHandler(HTTP11DownloadHandler):
+    """
+    https://gist.github.com/cydu/8a4b9855c5e21423c9c5
+    author: cydu<root@cydu.net>
+    """
+
+    def download_request(self, request, spider):
+        """Return a deferred for the HTTP download"""
+        agent = ScrapySocks5Agent(contextFactory=self._contextFactory, pool=self._pool)
+        return agent.download_request(request)
+
+
+class ScrapySocks5Agent(ScrapyAgent):
+    """
+    https://gist.github.com/cydu/8a4b9855c5e21423c9c5
+    author: cydu<root@cydu.net>
+    """
+
+    def _get_agent(self, request, timeout):
+        bindAddress = request.meta.get('bindaddress') or self._bindAddress
+        proxy = request.meta.get('proxy')
+        if proxy:
+            _, _, proxyHost, proxyPort, proxyParams = _parse(proxy)
+            _, _, host, port, proxyParams = _parse(request.url)
+            proxyEndpoint = TCP4ClientEndpoint(reactor, proxyHost, proxyPort,
+                                timeout=timeout, bindAddress=bindAddress)
+            agent = SOCKS5Agent(reactor, proxyEndpoint=proxyEndpoint)
+            return agent
+        return self._Agent(reactor, contextFactory=self._contextFactory,
+            connectTimeout=timeout, bindAddress=bindAddress, pool=self._pool)
 
 
 class TorProcess:
@@ -30,16 +78,21 @@ class TorProcess:
         print(" - Starting TOR Process...")
         self.socks_port = socks_port
         self.control_port = control_port
-        self.tor_processes.append(stem.process.launch_tor_with_config(
+        stem.process.launch_tor_with_config(
             config={
                 'SocksPort': str(self.socks_port),
                 'ControlPort': str(self.control_port+index),
                 'DataDirectory':'data/tor'+str(index),
                 'PidFile':'tor'+str(index)+'.pid',
                 },
-            init_msg_handler = msg_handler,
+            init_msg_handler = self.msg_handler,
             take_ownership = True,
-            ))
+            )
+        self.nym_updated_on = time.time()
+
+    def msg_handler(self, line):
+        if("Bootstrapped " in line):
+            print(line)
 
     def newnym(self):
         print("[+] Changing IP Address... [%i]" % self.socks_port)
@@ -50,30 +103,33 @@ class TorProcess:
         time.sleep(controller.get_newnym_wait())
         end = time.time()
         print (end - start)
+        self.nym_updated_on = time.time()
 
     def __unicode__(self):
         return unicode(proxy_address)
 
-    @attribute
+    @property
     def proxy_address(self):
         return '127.0.0.1:%i' % self.socks_port
 
 
 class TorPool(object):
+
     def __init__(self, size=1):
         self.tor_processes = []
         self.base_socks_port = 9000
         self.base_control_port = 7000
-        for i in xrange(size):
-            tor_processes.append(TorProcess(base_socks_port=self.base_socks_port+i,
-                                            base_control_port=self.base_control_port+i,
-                                            port_index=i))
+        for i in range(size):
+            self.tor_processes.append(TorProcess(
+                        socks_port=self.base_socks_port+i,
+                        control_port=self.base_control_port+i,
+                        index=i))
 
     def __getitem__(self, key):
         return self.tor_processes[key]
 
     def __len__(self):
-        return len(tor_processes)
+        return len(self.tor_processes)
 
 
 class TorProxy(object):
@@ -92,20 +148,22 @@ class TorProxy(object):
             return
         tor_proc = self._request_tor_instance(request)
         if 'newnym' in request.meta:
-            tor_proc.newnym()
-        request.meta['proxy'] = tor_proc.proxy_address
+            if request.meta['newnym'] > tor_proc.nym_updated_on:
+                tor_proc.newnym()
+        #print('prox_addr: %s' % tor_proc.proxy_address)
+        request.meta['proxy'] = 'socks5://%s' % tor_proc.proxy_address
 
-    def process_exception(self, request, exception, spider):
-        tor_proc = self._request_tor_instance(request)
-        proxy = request.meta['proxy']
-        log.msg('Failed proxy <%s>, %d proxies available' % (
-                    tor_proc, len(self.tor_pool)))
-        try:
-            pass
-        except ValueError:
-            pass
+    # def process_exception(self, request, exception, spider):
+    #     tor_proc = self._request_tor_instance(request)
+    #     proxy = request.meta['proxy']
+    #     log.msg('Failed proxy <%s>, %d proxies available' % (
+    #                 tor_proc, len(self.tor_pool)))
+    #     try:
+    #         pass
+    #     except ValueError:
+    #         pass
 
     def _request_tor_instance(self, request):
-        req_hash = request_fingerprint(request)
-        proxy_idx = req_hash % self.pool_size
-        return  self.tor_pool[proxy_idx]
+        fingerprint = request_fingerprint(request)
+        proxy_idx = int(fingerprint, 16) % len(self.tor_pool)
+        return self.tor_pool[proxy_idx]
